855c8b2c6fa284da99ee1232ea50ff388fbc3779
Ayushman Bisht <ayushmanbisht007@gmail.com>
2026-08-17 20:23:14 +0530

    update my c++ code

---
+++ test.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int a,result;
+    printf("enter the number:");
+    scanf("%d",&a);
+    result=a*a*a;
+    printf("the cube of %d is:%d",a,result);
+    return 0;
+}
